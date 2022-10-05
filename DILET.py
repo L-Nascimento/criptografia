41af9aefb3f406c2ed6bad3d50d6a87cb06206d0
Diego Ramos <diego@connect.com.vc>
2022-10-05 12:17:11 -0400

    Inclui metodo proprio de criptografia

---
+++ DILET.py
@@ -0,0 +1,18 @@
+import random  # Será usada para gerar uma senha aleatória
+
+
+class DILET:
+    const_caracters = ()
+    new_caracter_sequence = []
+
+    def __init__(self):
+        ...  # todo: inicializa classe
+
+    def create_password(self):
+        ...  # todo: Irá criar uma senha de criptografia
+
+    def encrypt(self, plain_text):
+        ...  # todo: Irá criptografar a palavra
+
+    def decrypt(self, encrypt_text):
+        ...  # todo: Irá descriptografar a palavra
--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 from cryptography.fernet import Fernet
-
+from DILET import DILET
 
 print("digite a frase para encriptar")
 original = input()
-
-key = Fernet.generate_key()
-fernet = Fernet(key)
-
-enc = fernet.encrypt(original.encode())
-
-desc = fernet.decrypt(enc).decode()
-
+key = Fernet.generate_key()  # todo: substituir por create_password do DILET
+fernet = Fernet(key)  # todo: no caso do DILET essa linha não será necessária
+enc = fernet.encrypt(original.encode())  # todo: substituir por encrypt do DILET
+desc = fernet.decrypt(enc).decode()  # todo: substituir pelo decrypy do DILET
 print("Mensagem original: ", original)
 print("Encriptada: ", enc)
 print("Descriptada: ", desc)
-
-
-
-
-
-
-
