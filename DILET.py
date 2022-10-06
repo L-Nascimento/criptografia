261dc876b7a4e9f83465835162c3d16e4e61535d
Aluno Invest <aluno.invest@invest.local>
2022-10-05 22:51:52 -0300

    Criptografia com ASCII

--- DILET.py
+++ DILET.py
@@ -1,18 +1,47 @@
 import random  # Será usada para gerar uma senha aleatória
+import string
 
 
 class DILET:
-    const_caracters = ()
-    new_caracter_sequence = []
+    def __init__(self):  # Inicializa classe
+        self.const_caracters = (
+            " ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+",
+            ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7",
+            "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C",
+            "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[",
+            "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g",
+            "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+            "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~")
+        self.new_caracter_sequence = []
 
-    def __init__(self):
-        ...  # todo: inicializa classe
+    def _fill_new_caracters(self):
+        for letter in self.const_caracters:
+            if not self.new_caracter_sequence.__contains__(letter):
+                self.new_caracter_sequence.append(letter)
 
-    def create_password(self):
-        ...  # todo: Irá criar uma senha de criptografia
+    def create_password(self):  # Cria a senha para a criptografia
+        password = ""
+        for i in range(0, 52):
+            password += random.choice(string.ascii_letters)
+        return password
 
-    def encrypt(self, plain_text):
-        ...  # todo: Irá criptografar a palavra
+    def encrypt(self, plain_text, password):  # Encripita texto enviado
+        encrypt_text = ""
+        self.new_caracter_sequence.clear()
+        for letter in password:
+            self.new_caracter_sequence.append(self.const_caracters[self.const_caracters.index(letter)])
+        self._fill_new_caracters()
+        for letter in plain_text:
+            encrypt_text += self.new_caracter_sequence[self.const_caracters.index(letter)]
+        return encrypt_text
 
-    def decrypt(self, encrypt_text):
-        ...  # todo: Irá descriptografar a palavra
+    def decrypt(self, encrypt_text, password):
+        decrypt_text = ""
+        self.new_caracter_sequence.clear()
+        for letter in password:
+            self.new_caracter_sequence.append(self.const_caracters[self.const_caracters.index(letter)])
+        self._fill_new_caracters()
+        for letter in encrypt_text:
+            decrypt_text += self.const_caracters[self.new_caracter_sequence.index(letter)]
+        return decrypt_text
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from cryptography.fernet import Fernet
 from DILET import DILET
 
-print("digite a frase para encriptar")
-original = input()
-key = Fernet.generate_key()  # todo: substituir por create_password do DILET
-fernet = Fernet(key)  # todo: no caso do DILET essa linha não será necessária
-enc = fernet.encrypt(original.encode())  # todo: substituir por encrypt do DILET
-desc = fernet.decrypt(enc).decode()  # todo: substituir pelo decrypy do DILET
-print("Mensagem original: ", original)
-print("Encriptada: ", enc)
-print("Descriptada: ", desc)
+print("=-" * 20)
+cripto = DILET()
+password = cripto.create_password()
+plain_text = input("Digite a frase para encriptar: \n")
+encrypt_text = cripto.encrypt(plain_text, password)
+decrypt_text = cripto.decrypt(encrypt_text, password)
+print(f"Mensagem original: {plain_text}"
+      f"\nSenha de criptografia: {password}"
+      f"\nEncriptada: {encrypt_text}"
+      f"\nDecriptada: {decrypt_text}")
